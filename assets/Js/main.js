67d8288ca43d3f9b094aa78b34f532656618b879
Ahmed-517 <ahmed195041@feng.bu.edu.eg>
2022-08-08 02:32:44 +0200

    add header section inside index.html

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,12 +1,29 @@
 /*=============== SHOW MENU ===============*/
-
+const navMenu = document.querySelector('#nav-menu')
+// const navToggle = document.getElementById('nav-toggle');
+const navClose = document.querySelector('#nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+function showMenu() {
+    navMenu.classList.add('show-menu');
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if (navClose) {
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu');
+    })
+}
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+
+window.addEventListener('scroll', () => {
+    const haeder = document.querySelector('#header');
+
+    if (this.scrollY >= 100)
+        haeder.classList.add('scroll-header');
+    else
+        haeder.classList.remove('scroll-header');
+})
