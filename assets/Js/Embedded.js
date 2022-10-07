6843139e7279a575b9f941198573d02826b87125
Mahmoud Khalaf <Mahmoud21196@feng.bu.edu.eg>
2022-10-07 22:35:11 +0200

    Embedded**Header&footer

--- assets/Js/Embedded.js
+++ assets/Js/Embedded.js
@@ -3,12 +3,12 @@ let pic2 =document.getElementById("im2").style
 let pic3 =document.getElementById("im3").style
 window.onscroll= function(){
     console.log(window.scrollY)
-    if(window.scrollY >= 160){
+    if(window.scrollY >= 260){
         document.getElementById("im1").style="animation-play-state: running ;"
-    }if(window.scrollY >= 360){
+    }if(window.scrollY >= 460){
         document.getElementById("im2").style="animation-play-state: running ;"
     }
-    if(window.scrollY >= 610){
+    if(window.scrollY >= 710){
         document.getElementById("im3").style="animation-play-state: running ;"
     }
 }
