56c010162b99c58bc38a99cd60fbbc8c691f1c01
Mahmoud Khalaf <Mahmoud21196@feng.bu.edu.eg>
2022-10-08 00:00:40 +0200

    modications

--- assets/Js/Embedded.js
+++ assets/Js/Embedded.js
@@ -5,7 +5,7 @@ window.onscroll= function(){
     console.log(window.scrollY)
     if(window.scrollY >= 260){
         document.getElementById("im1").style="animation-play-state: running ;"
-    }if(window.scrollY >= 460){
+    }if(window.scrollY >= 485){
         document.getElementById("im2").style="animation-play-state: running ;"
     }
     if(window.scrollY >= 710){
